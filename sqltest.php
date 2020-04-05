97a931d5a6ac4b18b597492d6071cfb07a2bb45d
Benjamin Spain <benjamin@thespains.net>
2020-04-05 16:52:15 -0500

    sqltest file

---
+++ sqltest.php
@@ -0,0 +1,18 @@
+<?php
+    include 'auth.php';
+
+    error_log("Called Auth: $DB_USER $DB_PASS $DB_ADDRESS $DB_SCHEMA");
+
+    $conn = mysqli_connect($DB_ADDRESS, $DB_USER, $DB_PASS, $DB_SCHEMA);
+    $query = "SELECT SYS_ID, TITLE, TARGET_AUDIENCE, MEET_DAY, MEET_TIME_START, DURATION, LEADER, PHONE_NUMBER, EMAIL, LOCATION, CAMPUS, GROUP_LINK, GROUP_TYPE, CARE_PROVIDED, COST, ACTIVE, TRIM(DESCRIPTION) as DESCRIPTION FROM SMALL_GROUPS LIMIT 1";
+    $result = mysqli_query($conn, $query) or die('{"records": [{"error": "' . mysqli_error($connection) . '"}]}');
+    
+    $info = array();
+
+    while($rs = mysqli_fetch_assoc($result)) {
+        //array_push($info, $rs);
+        array_push($info, $rs);
+    } 
+
+    echo json_encode($info);
+?> 
