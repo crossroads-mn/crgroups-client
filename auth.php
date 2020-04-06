32e41b5fbe32df74ebfcc38a3ec0f7a28e31914e
Benjamin Spain <benjamin@thespains.net>
2020-04-05 19:51:58 -0500

    Check server and env - take 2

--- auth.php
+++ auth.php
@@ -2,26 +2,24 @@
 	$tmp = explode('.', $_SERVER['HTTP_HOST']);
 	$subdomain = current($tmp);
 
-	$DB_ADDRESS = "";
-    $DB_USER = "";
-    $DB_PASS = "";
-    $DB_SCHEMA = "";
-
-	// Parsing connection string
-	$MYSQL_CONN_STRING = "";
-	if ($_SERVER["MYSQL_CONN_STRING"] !== 0) {
-		$MYSQL_CONN_STRING = $_SERVER["MYSQL_CONN_STRING"];
-	}
-	else if ($_ENV["MYSQL_CONN_STRING"] !== 0) {
-		$MYSQL_CONN_STRING = $_ENV["MYSQL_CONN_STRING"];
+	// Parse connection string
+	if ( !isset($MYSQL_CONN_STRING) || empty($MYSQL_CONN_STRING)) {
+		if ( isset($_SERVER["MYSQL_CONN_STRING"]) && !empty($_SERVER["MYSQL_CONN_STRING"]) ) {
+			$MYSQL_CONN_STRING = $_SERVER["MYSQL_CONN_STRING"];
+		}
+		else if ( isset($_ENV["MYSQL_CONN_STRING"]) && !empty($_ENV["MYSQL_CONN_STRING"]) ) {
+			$MYSQL_CONN_STRING = $_ENV["MYSQL_CONN_STRING"];
+		}	
 	}
 
-	if ($MYSQL_CONN_STRING !==0 ) {
-		$DB_ADDRESS = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-		$DB_SCHEMA = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-		$DB_USER = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-		$DB_PASS = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+	if (!isset($MYSQL_CONN_STRING)) {
+		die("Unable to find MYSQL_CONN_STRING");
 	}
+
+	$DB_ADDRESS = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $MYSQL_CONN_STRING);
+	$DB_SCHEMA = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $MYSQL_CONN_STRING);
+	$DB_USER = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $MYSQL_CONN_STRING);
+	$DB_PASS = preg_replace("/^.*Password=(.+?)$/", "\\1", $MYSQL_CONN_STRING);
 
 	$GLOBAL_DB_ADDRESS = $DB_ADDRESS;
 	$GLOBAL_DB_USER = $DB_USER;
